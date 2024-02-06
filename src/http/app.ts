61343969eeff509c926d48744b22335235ef6b76
Luiz Gustavo <gusstavopdm@gmail.com>
2024-02-06 20:37:29 -0300

    ✨ feat(app.ts): register cookie plugin

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,9 +1,15 @@
 import fastify from 'fastify'
+import cookie from '@fastify/cookie'
 
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
 
 const app = fastify()
+
+app.register(cookie, {
+  secret: 'secret',
+  hook: 'onRequest',
+})
 
 app.register(createPoll)
 app.register(getPoll)
