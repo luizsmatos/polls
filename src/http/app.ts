89c2fbb154b0502438fd6e3448f588c07393f1ea
Luiz Gustavo <gusstavopdm@gmail.com>
2024-02-06 20:20:13 -0300

    ✨ feat(app.ts): add support for get-poll route to enable retrieving poll data

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,8 +1,11 @@
 import fastify from 'fastify'
+
 import { createPoll } from './routes/create-poll'
+import { getPoll } from './routes/get-poll'
 
 const app = fastify()
 
 app.register(createPoll)
+app.register(getPoll)
 
 export { app }
