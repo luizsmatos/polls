dde21eedcd57220168353b0a8421f901d6d24140
Luiz Gustavo <gusstavopdm@gmail.com>
2024-02-08 11:23:25 -0300

    ✨ feat(app.ts): register the websocket plugin to enable real-time communication

--- src/http/app.ts
+++ src/http/app.ts
@@ -1,5 +1,6 @@
 import fastify from 'fastify'
 import cookie from '@fastify/cookie'
+import websocket from '@fastify/websocket'
 
 import { createPoll } from './routes/create-poll'
 import { getPoll } from './routes/get-poll'
@@ -11,6 +12,8 @@ app.register(cookie, {
   secret: 'secret',
   hook: 'onRequest',
 })
+
+app.register(websocket)
 
 app.register(createPoll)
 app.register(getPoll)
